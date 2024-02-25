e7acfcc118c9f8886974683f05a4818c0e038f50
Yash-Raj-Varma <88896207+YashRaj011@users.noreply.github.com>
2024-02-25 14:37:21 +0530

    Done by Yash Raj - 25/02/24

--- src/main/java/com/example/Employee/Training/Management/System/Controller/LoginController.java
+++ src/main/java/com/example/Employee/Training/Management/System/Controller/LoginController.java
@@ -1,40 +1,22 @@
 package com.example.Employee.Training.Management.System.Controller;
 
-import com.example.Employee.Training.Management.System.Repository.UserRepository;
-import com.example.Employee.Training.Management.System.model.User;
-import com.example.Employee.Training.Management.System.service.LoginService;
  import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.ModelAttribute;
  import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
-import java.util.Objects;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
- @Slf4j
+@Slf4j
 @Controller
 public class LoginController {
-     private final LoginService logservice;
-     private UserRepository userRepository;
-
-    @Autowired
-    public LoginController(LoginService logservice, UserRepository userRepository) {
-        this.logservice = logservice;
-        this.userRepository = userRepository;
-    }
 
     @RequestMapping(value = "/login", method = {RequestMethod.GET, POST})
     public String displayLoginPage(@RequestParam(value = "error", required = false) String error,
@@ -72,17 +54,5 @@ public class LoginController {
         }
         return "redirect:/login?logout=true";
     }
-
-//    @RequestMapping(value = {"/checkMsg"}, method = POST)
-//    public String validateUser(@Valid @ModelAttribute("login_user")LogUser loguser, Errors errors, Model model) {
-//        if(errors.hasErrors()){
-//            model.addAttribute("userDetail", "failed");
-//            log.error("User login failed due to : " + errors.toString());
-//            return "Login.html";
-//        }
-//        return "/CompanyDetailsRegistration";
-//    }
-
-
 
 }
